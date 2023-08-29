a237e086ff7142f75848343b152b9f9b5d4038e6
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-29 21:58:57 +0300

    Add Weather Form Component

---
+++ src/components/WeatherForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Row, Col, Form, Button} from 'react-bootstrap'
+
+const WeatherForm = () => {
+  return (
+      <Row>
+          <Col>
+              <Form>
+                  <h1>Form Header</h1>
+            </Form>
+          </Col>
+    </Row>
+  )
+}
+
+export default WeatherForm

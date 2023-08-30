4b48bfddaf7a205f62a9b5761794b7e63027d914
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-30 20:21:17 +0300

    Add Fetch Function To LoadOptions

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,4 @@
-
-
-
-export const GOE_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const geoApiOptions = {
     method: 'GET',
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
+import React, { useState } from 'react';
+import { AsyncPaginate } from 'react-select-async-paginate';
+import { GEO_API_URL, geoApiOptions } from './Api'
 
 const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+            .then((response) => response.json())
+            .then((response) => console.log(response))
+            .catch((err) => console.log(err));
     }
 
     const handleChange = (searchData) => {

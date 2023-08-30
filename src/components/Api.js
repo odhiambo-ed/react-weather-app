760c371d8aae5184a96f38435bdc2efb7c72bf8c
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-30 19:50:20 +0300

    Add API Component

---
+++ src/components/Api.js
@@ -0,0 +1,12 @@
+
+
+
+export const GOE_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '716f238d96msha65ec4c38fa151ap191c77jsn318966762a7e',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
